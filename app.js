5bf06d70e9c5ce2804de3a5ca184de6da406b3e1
Mang506 <mrubly@live.com>
2019-09-01 17:47:35 -0700

    Seperated modules and added event emitters

---
+++ app.js
@@ -0,0 +1,8 @@
+const name = require("./myModule");
+
+name.inc();
+name.inc();
+name.inc();
+name.dec();
+
+console.log(name.getCount());
--- questions.js
+++ questions.js
@@ -1,9 +1,4 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const collectAnswers = require("./lib/collectAnswers")
 
 const questions = [
   "What is your name? ",
@@ -11,29 +6,15 @@ const questions = [
   "What are you doing with node js? "
 ];
 
-const collectAnswers = (questions, done) => {
-  const answers = [];
-  const [firstQuestion] = questions;
-  
-  const questionAnswered = answer => {
-    // stores all answers
-    answers.push(answer);
-    // asks second question through last
-    if (answers.length < questions.length){
-      rl.question(questions[answers.length], questionAnswered);
-    } else {
-      // all questions answers
-      done(answers);
-    }
+const answerEvents = collectAnswers(questions);
 
-  }
+answerEvents.on("answer", answer => {
+  console.log(`qustion answered: ${answer}`);
+});
 
-  // asks first question then callback function continues the remaining
-  rl.question(firstQuestion, questionAnswered);
-};
-
-collectAnswers(questions, answers => {
+answerEvents.on("complete", answers => {
   console.log("Thank you for your answers. ");
   console.log(answers);
-  process.exit();
 });
+
+answerEvents.on("complete", () => process.exit());
